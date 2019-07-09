8d9f28d113db8786c435ad5b6baa09fe463e0e5c
David Escribano Rodriguez <david.escribanor@gmail.com>
2019-07-09 12:59:05 +0200

    Rules checked, poker seems not perfect

--- kata_3/game.mjs
+++ kata_3/game.mjs
@@ -21,68 +21,78 @@ function playGame() {
 
     // Check
 
-    checkColor(hand1);
-    checkPoker(hand1);
-    getHigherCardValue(hand1);
+    hand1 = Array.from(hand1.cardList);
+
+    console.log(`checkStraightFlush? ${checkStraightFlush(hand1)}`);
+    console.log(`checkPoker? ${checkPoker(hand1)}`);
+    console.log(`checkFull? ${checkFull(hand1)}`);
+    console.log(`checkFlush? ${checkFlush(hand1)}`);
+    console.log(`checkStraight? ${checkStraight(hand1)}`);
+    console.log(`checkThreeOfAKind? ${checkThreeOfAKind(hand1)}`);
+    console.log(`checkDoublePair? ${checkDoublePair(hand1)}`);
+    console.log(`checkPair? ${checkPair(hand1)}`);
+    console.log(`getHigherCardValue = ${getHigherCardValue(hand1)}`);
 }
 
 
 
 function init() {
-    // let cardSet1 = new Set([
-    //     new Card( '1', 'H' ),
-    //     new Card( '2', 'S' ),
-    //     new Card( '3', 'H' ),
-    //     new Card( '9', 'C' ),
-    //     new Card( '2', 'D' )
-    // ].sort(lowestToHighest));
-    // let cardSet2 = new Set([
-    //     new Card( '2', 'C' ),
-    //     new Card( '3', 'H' ),
-    //     new Card( '4', 'S' ),
-    //     new Card( '8', 'C' ),
-    //     new Card( 'A', 'H' )
-    // ]);
     let cardSet1 = new Set([
         new Card( '1', 'H' ),
-        new Card( '8', 'S' ),
-        new Card( '3', 'H' ),
-        new Card( '6', 'C' ),
-        new Card( '5', 'D' )
+        new Card( '1', 'S' ),
+        new Card( '1', 'D' ),
+        new Card( '1', 'C' ),
+        new Card( '2', 'H' )
     ].sort(lowestToHighest));
-
 
     hand1 = new Hand(cardSet1);
     // hand2 = new Hand(cardSet2);
 }
 
-function checkColor( hand ) {
-    let color = hand.cardList.values().next().value.suit; // First card color of the hand
-    let isColor = true;
 
-    hand.cardList.forEach(card => {
-        if ( card.suit !== color ) 
-            isColor = false;
-    });
-    console.log('Color? ' + isColor);
-    return isColor;
+function checkStraightFlush( hand ) {
+    let isStraightFlush = checkStraight(hand) && checkFlush(hand);
+    return isStraightFlush;
 }
 
+function checkStraight( hand ) {
+    let initialValue = hand[0].value;
+    let isStraight = true;
+
+    hand.slice(1).forEach(card => {
+        if( parseInt(card.value) !== ( parseInt(initialValue) + 1 ) ) {
+            isStraight = false;
+        }
+        initialValue = card.value;
+    });
+    return isStraight;
+}
+
+function checkFlush( hand ) {
+    let suit = hand[0].suit; // First card color of the hand
+    let isFlush = true;
+
+    hand.forEach(card => {
+        if ( card.suit !== suit ) 
+            isFlush = false;
+    });
+    return isFlush;
+}
 
 function checkPoker( hand ) {
-    let baseCard = hand.cardList.values().next().value; // First card value of the hand
+    let baseCard = hand[0].value; // First card value of the hand
     let counter = 0;
     let changeCardValue = 0;
     let isPoker = true;
 
-    let subArray = Array.from(hand.cardList).slice(1);
+    let subArray = hand.slice(1);
 
     for(let i = 0; i <= subArray.length; i++) {
         let currentCard = subArray[i];
         
         if( counter === 3 && i >= 2 ) {
             break; // poker = true (exit condition)
-        } else if ( currentCard === undefined || (counter <= 2 && i > 2 && changeCardValue > 1)  ) { // Last check failed or impossible to reach a poker
+        } else if ( currentCard === undefined || (counter <= 2 && i > 2 && changeCardValue > 1) ) { // Last check failed or impossible to reach a poker
             isPoker = false; // poker = false (exit condition)
             break;
         } else if ( baseCard.value === currentCard.value ) {
@@ -93,15 +103,68 @@ function checkPoker( hand ) {
             counter = 0;
         }
     }
-    console.log('Poker? ' + isPoker);
 
     return isPoker;
 }
 
+function checkFull( hand ) {
+    let isFull = false;
+    let handWithoutThree = getUnrepeatedCards( hand, 3 );
+    if(handWithoutThree !== null) // If there is a three of a kind, check if there is a pair too
+        isFull = checkPair( handWithoutThree );
 
+    return isFull;
+}
+
+function checkThreeOfAKind( hand ) {
+    return isRepeatedInANumber( hand, 3);
+}
+
+function checkDoublePair( hand ) {
+    let isDoublePair = false;
+    let unrepeatedCards = getUnrepeatedCards(hand, 2);
+    if(unrepeatedCards !== null) // If at least a pair, we check if there is another one
+        isDoublePair = checkPair( unrepeatedCards );
+    return isDoublePair;
+}
+
+function checkPair( hand ) {
+    return isRepeatedInANumber( hand, 2);
+}
 
 function getHigherCardValue( hand ) {
-    let cardValue = Array.from(hand.cardList)[4].value;
-    console.log( `Higher card is: ${cardValue}` );
+    let cardValue = hand[4].value;
     return cardValue;
+}
+
+
+
+// Aux methods
+
+
+function getUnrepeatedCards( hand, repeatedAmount ) {
+    let otherCards = null;
+    for(let i = 0; i < hand.length; i++) {
+        let currentCard = hand[i];
+        let repeatedCards = hand.filter( card => card.value === currentCard.value );
+        if( repeatedCards.length >= repeatedAmount ) {
+
+            otherCards = hand.slice(); 
+            otherCards.splice(i, repeatedAmount);
+            break;
+        }
+    }
+    return otherCards;
+}
+
+function isRepeatedInANumber( hand, repeatedAmount ) {
+    let isRepeated = false;
+
+    hand.forEach( currentCard => {
+        let possibleRepeated = hand.filter( card => card.value === currentCard.value );
+        if( possibleRepeated.length >= repeatedAmount ) {
+            isRepeated = true;
+        }
+    });
+    return isRepeated;
 }
